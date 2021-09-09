085cf26fb082290b0d8a1b991aa3cc5c787ddf21
Ankit <48017526+ankit6271@users.noreply.github.com>
2021-09-10 01:16:29 +0530

    To concatenate Array

--- Easy/ArrayConcatenation.js
+++ Easy/ArrayConcatenation.js
@@ -1,1 +1,12 @@
+import java.util.*;
+class ArrayConcatenation {
+    public int[] getConcatenation(int[] nums) {
+        int ans[]=new int[2*nums.length];
+        for(int i=0;i<nums.length;i++){
+            ans[i]=nums[i];
+            ans[i+nums.length]=nums[i];
+        }
+        return ans;
+    }
+}
 
