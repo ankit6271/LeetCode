f211167d6fb7dc19bb5d56f4e957359f3fbacc6d
Ankit <48017526+ankit6271@users.noreply.github.com>
2021-09-14 01:23:29 +0530

    Shuffle [x1,...,xn,y1,..,yn] => [x1,y1,...,xn,yn]

---
+++ Easy/ArrayShuffle.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int shuff []=new int [nums.length];
+        int j=0;
+        for(int i=0;i<n;i++){
+            shuff[j]=nums[i];
+            shuff[++j]=nums[i+n];
+            j++;
+        }
+        return shuff;
+    }
+}
